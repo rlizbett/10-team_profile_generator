d7998fa29140d82bca27617d046064cc99333ed7
lizbett <rlizbett21@gmail.com>
2022-02-02 13:01:23 -0600

    employee.test.js test added

---
+++ _test_/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("New", () => {
+        it("test that should create new employee ben", () => {
+            const employee = new Employee("ben", 21, "bensandlin@gmail.com");
+            expect(employee.name).toEqual("ben");
+            expect(employee.id).toEqual(21);
+            expect(employee.email).toEqual("bensandlin@gmail.com");
+        });
+    });
+});
