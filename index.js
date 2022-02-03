d0a56e6ea82afa516bb43fe24e147ac84a2c503a
lizbett <rlizbett21@gmail.com>
2022-02-03 16:51:54 -0600

    index.js questions added

--- index.js
+++ index.js
@@ -9,4 +9,121 @@ const Manager = require("./lib/Manager");
 const employeeMembers = [];
 const idNum = [];
 
+//functions for Employee, Manager, Engineer, and Intern
+function managerQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Manager's name?",
+            name: "ManagerName",
+        },
+        {
+            type: "input",
+            message: "What is the Manager's Id?",
+            name: "ManagerId",
+        },
+        {
+            type: "input",
+            message: "What is the Manager's email adress?",
+            name: "ManagerEmail",
+        },
+        {
+            type: "input",
+            message: "What is the Manager's office number?",
+            name: "ManagersOffNum",
+        },
+    ])
+        .then((response) => {
+            console.log(response);
+            employeeMembers.push(new Manager(response));
+            employeeQuestions();
+        });
 
+};
+//Engineer function
+function engineerQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Engineer's name?",
+            name: "EngineerName",
+        },
+        {
+            type: "input",
+            message: "What is the Engineer's Id?",
+            name: "EngineerId",
+        },
+        {
+            type: "input",
+            message: "What is the Engineer's email adress?",
+            name: "EngineerEmail",
+        },
+        {
+            type: "input",
+            message: "What is the Engineers github username?",
+            name: "EngineerGithub",
+        },
+
+    ])
+        .then((response) => {
+            console.log(response);
+            employeeMembers.push(new Engineer(response));
+            employeeQuestions();
+        });
+};
+//Add employees
+function employeeQuestions() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Which employee would you like to add?",
+            name: "EployeeChoice",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I dont wish to add more Employees"
+            ]
+        }
+    ])
+        .then((response) => {
+            console.log(response);
+            if (response.EmployeeChoice == "Engineer") {
+                engineerQuestions();
+            }
+            else if (response.EmployeeChoice == "Intern") {
+                internQuestions();
+            }
+            else {
+                fs.writeFile('./src/htmlDemo.html', pageHTML(employeeMembers), (err) =>
+                    err ? console.log(err) : console.log('responces recorded to index.html'))
+                console.log(employeeMembers);
+            }
+        });
+};
+//Intern function
+function internQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Intern's name?",
+            name: "InternName",
+        },
+        {
+            type: "input",
+            message: "What is the Intern's Id?",
+            name: "InternId",
+        },
+        {
+            type: "input",
+            message: "What is the Intern's email adress?",
+            name: "InternEmail",
+        },
+        {
+            type: "input",
+            message: "What school does the intern attend?",
+            name: "InternSchool",
+        },
+
+    ]);
+};
+managerQuestions();
